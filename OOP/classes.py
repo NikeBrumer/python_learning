5429e950ca6cb26a1774151bac1a0d4d0ca749af
Nike Brumer <Nowllinks@yandex.ru>
2023-05-15 16:26:50 +0300

    learned function getattr()

--- OOP/classes.py
+++ OOP/classes.py
@@ -12,12 +12,27 @@ class Goods:
     tp = "Еда"
     price = 1024
 
+
 class Car:
     pass
-#setattr(Class, 'attribute', value) - добавляет атрибут и его значение в указанный класс
+
+
+# setattr(Class, 'attribute', value) - добавляет атрибут и его значение в указанный класс
 
 setattr(Car, 'model', 'Тойота')
 setattr(Car, 'color', 'Розовый')
 setattr(Car, 'number', 'П111УУ77')
 
 print(Car.__dict__['color'])
+
+
+class Notes:
+    uid = 1005435
+    title = "Шутка"
+    author = "И.С. Бах"
+    pages = 2
+
+
+# getattr(Class, attribute, returned_value(optional)) - получает значение атрибута, если он существует и None или
+# значение в обратной ситуации
+print(getattr(Notes, 'author'))
