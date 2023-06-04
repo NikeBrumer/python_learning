c6e2fffa41904ec1070b1b6f61d943dbf5f6bea5
Nike Brumer <Nowllinks@yandex.ru>
2023-06-04 16:57:56 +0300

    descriptors (data)

--- OOP/classes.py
+++ OOP/classes.py
@@ -482,3 +482,44 @@ router.link(ser2)
 ser.send_data(Data('abc', ser2.get_ip()))
 router.send_data()
 
+
+
+#04/06/2023
+
+class FloatValue:
+    @classmethod
+    def check_float(cls, value):
+        if not type(value) == float:
+            raise TypeError("Присваивать можно только вещественный тип данных.")
+
+    def __set_name__(self, owner, name):
+        self.name = '_' + name
+
+    def __get__(self, instance, owner):
+        return getattr(instance, self.name)
+
+    def __set__(self, instance, value):
+        self.check_float(value)
+        setattr(instance, self.name, value)
+
+
+class Cell:
+    value = FloatValue()
+
+    def __init__(self, value=0.0):
+        self.value = value
+
+
+class TableSheet:
+    def __init__(self, N, M):
+        self.N = N
+        self.M = M
+        self.cells = [[Cell() for j in range(M)] for i in range(N)]
+
+
+table = TableSheet(5, 3)
+z = 1.0
+for i in range(table.N):
+    for j in range(table.M):
+        table.cells[i][j].value = z
+        z += 1
